70d15d221161d9065a2bdb98cfac8a6c78fa7d8f
Darren Ehlers <me@darrenehlers.com>
2013-11-19 16:18:15 -0600

    Fixed DNLabel EdgeInsets bug
    - Fixed DNLabel EdgeInsets bug
    - Updated podspec version
    
    Signed-off-by: Darren Ehlers <me@darrenehlers.com>

--- Classes/DNLabel.m
+++ Classes/DNLabel.m
@@ -8,11 +8,14 @@
 
 #import "DNLabel.h"
 
+#import "DNUtilities.h"
+
 @implementation DNLabel
 
 - (void) drawTextInRect:(CGRect)rect
 {
-    UIEdgeInsets insets = { rect.origin.x + self.horizontalPadding, rect.origin.y + self.verticalPadding, rect.size.width - self.horizontalPadding * 2, rect.size.height - self.verticalPadding * 2};
+    UIEdgeInsets insets = UIEdgeInsetsMake(self.verticalPadding, self.horizontalPadding, self.verticalPadding, self.horizontalPadding);
+    DLog(LL_Debug, LD_Theming, @"insets=(%.2f, %.2f, %.2f, %.2f)", insets.top, insets.left, insets.bottom, insets.right);
 
     [super drawTextInRect:UIEdgeInsetsInsetRect(rect, insets)];
 }
