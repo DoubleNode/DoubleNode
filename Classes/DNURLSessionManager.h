0af3d9d000ec5f0e1ed908c8331fe19f0e0a445e
Darren Ehlers <me@darrenehlers.com>
2016-07-05 15:58:41 -0500

    Added data file uploads to DNURLSessionManager
    - Added data file uploads to DNURLSessionManager
    - Updated podspec version
    
    Signed-off-by: Darren Ehlers <me@darrenehlers.com>

--- Classes/DNURLSessionManager.h
+++ Classes/DNURLSessionManager.h
@@ -18,11 +18,19 @@
 
 - (_Nonnull instancetype)initWithSessionConfiguration:(NSURLSessionConfiguration* _Nullable)configuration;
 
-- (NSURLSessionDataTask* _Nonnull)dataTaskWithRequest:(NSURLRequest* _Nonnull)request
+- (NSURLSessionDataTask* _Nonnull)sendTaskWithRequest:(NSURLRequest* _Nonnull)request
                                    serverErrorHandler:(void(^ _Nullable)(NSHTTPURLResponse* _Nullable httpResponse))serverErrorHandler
                                      dataErrorHandler:(void(^ _Nullable)(NSData* _Nullable errorData, NSString* _Nullable errorMessage))dataErrorHandler
                                   unknownErrorHandler:(void(^ _Nullable)(NSError* _Nullable dataError))unknownErrorHandler
                                 noResponseBodyHandler:(void(^ _Nullable)())noResponseBodyHandler
-                                    completionHandler:(void(^ _Nullable)(NSURLResponse* _Nonnull response, id _Nullable responseObject, NSError* _Nullable error))completionHandler;
+                                    completionHandler:(void(^ _Nullable)(NSURLResponse* _Nonnull response, id _Nullable responseObject))completionHandler;
+
+- (NSURLSessionDataTask* _Nonnull)dataTaskWithRequest:(NSURLRequest* _Nonnull)request
+                                             withData:(NSData* _Nonnull)data
+                                   serverErrorHandler:(void(^ _Nullable)(NSHTTPURLResponse* _Nullable httpResponse))serverErrorHandler
+                                     dataErrorHandler:(void(^ _Nullable)(NSData* _Nullable errorData, NSString* _Nullable errorMessage))dataErrorHandler
+                                  unknownErrorHandler:(void(^ _Nullable)(NSError* _Nullable dataError))unknownErrorHandler
+                                noResponseBodyHandler:(void(^ _Nullable)())noResponseBodyHandler
+                                    completionHandler:(void(^ _Nullable)(NSURLResponse* _Nonnull response, id _Nullable responseObject))completionHandler;
 
 @end
--- Classes/DNURLSessionManager.m
+++ Classes/DNURLSessionManager.m
@@ -38,12 +38,12 @@
     return [super initWithSessionConfiguration:configuration];
 }
 
-- (NSURLSessionDataTask*)dataTaskWithRequest:(NSURLRequest*)request
+- (NSURLSessionDataTask*)sendTaskWithRequest:(NSURLRequest*)request
                           serverErrorHandler:(void(^ _Nullable)(NSHTTPURLResponse* _Nullable httpResponse))serverErrorHandler
                             dataErrorHandler:(void(^ _Nullable)(NSData* _Nullable errorData, NSString* _Nullable errorMessage))dataErrorHandler
                          unknownErrorHandler:(void(^ _Nullable)(NSError* _Nullable dataError))unknownErrorHandler
                        noResponseBodyHandler:(void(^ _Nullable)())noResponseBodyHandler
-                           completionHandler:(void(^ _Nullable)(NSURLResponse* _Nonnull response, id _Nullable responseObject, NSError* _Nullable error))completionHandler
+                           completionHandler:(void(^ _Nullable)(NSURLResponse* _Nonnull response, id _Nullable responseObject))completionHandler
 {
     return [super dataTaskWithRequest:request
                     completionHandler:
@@ -97,7 +97,71 @@
                     return;
                 }
                 
-                completionHandler ? completionHandler(response, responseObject, dataError) : nil;
+                completionHandler ? completionHandler(response, responseObject) : nil;
+            }];
+}
+
+- (NSURLSessionDataTask*)dataTaskWithRequest:(NSURLRequest*)request
+                                    withData:(NSData* _Nonnull)data
+                          serverErrorHandler:(void(^ _Nullable)(NSHTTPURLResponse* _Nullable httpResponse))serverErrorHandler
+                            dataErrorHandler:(void(^ _Nullable)(NSData* _Nullable errorData, NSString* _Nullable errorMessage))dataErrorHandler
+                         unknownErrorHandler:(void(^ _Nullable)(NSError* _Nullable dataError))unknownErrorHandler
+                       noResponseBodyHandler:(void(^ _Nullable)())noResponseBodyHandler
+                           completionHandler:(void(^ _Nullable)(NSURLResponse* _Nonnull response, id _Nullable responseObject))completionHandler
+{
+    return [super dataTaskWithRequest:request
+                    completionHandler:
+            ^(NSURLResponse* _Nonnull response, id _Nullable responseObject, NSError* _Nullable dataError)
+            {
+                if (dataError)
+                {
+                    DLog(LL_Info, LD_Networking, @"DATAERROR - %@", response.URL);
+                    
+                    if ([response isKindOfClass:[NSHTTPURLResponse class]])
+                    {
+                        NSHTTPURLResponse*    httpResponse    = (NSHTTPURLResponse*)response;
+                        if (httpResponse.statusCode == 500)
+                        {
+                            DLog(LL_Info, LD_Networking, @"WILLRETRY - %@", response.URL);
+                            [DNUtilities runOnBackgroundThreadAfterDelay:1.0f
+                                                                   block:
+                             ^()
+                             {
+                                 serverErrorHandler ? serverErrorHandler(httpResponse) : nil;
+                             }];
+                            return;
+                        }
+                    }
+                    
+                    NSData*    errorData   = dataError.userInfo[@"com.alamofire.serialization.response.error.data"];
+                    NSString*  errorString = [[NSString alloc] initWithData:errorData
+                                                                   encoding:NSASCIIStringEncoding];
+                    DLog(LL_Debug, LD_General, @"data=%@", errorString);
+                    
+                    if (errorData)
+                    {
+                        id jsonData = [NSJSONSerialization JSONObjectWithData:errorData
+                                                                      options:0
+                                                                        error:nil];
+                        if (jsonData)
+                        {
+                            NSString*  errorMessage    = jsonData[@"error"];
+                            
+                            dataErrorHandler ? dataErrorHandler(errorData, errorMessage) : nil;
+                        }
+                    }
+                    
+                    unknownErrorHandler ? unknownErrorHandler(dataError) : nil;
+                    return;
+                }
+                
+                if (!responseObject)
+                {
+                    noResponseBodyHandler ? noResponseBodyHandler() : nil;
+                    return;
+                }
+                
+                completionHandler ? completionHandler(response, responseObject) : nil;
             }];
 }
 
